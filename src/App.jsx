30ff017aac1a3077f2bd18cc5992688f5916b5ab
Sanju Devadas <sanjudevadas11@gmail.com>
2024-07-14 13:28:36 +0530

    completed the design of side bar now designing player

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
 
 function App() {
  
   return (
-    <>
-      
-    </>
+   <div className='h-screen bg-black'>
+<div className='h-[90%] flex'>
+<Sidebar/>
+</div>
+<div>
+  <Player/>
+</div>
+   </div>
   )
 }
 
