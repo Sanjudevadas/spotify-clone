5227d9570ee816b4dc4275bceca44c151a5bb636
Sanju Devadas <sanjudevadas11@gmail.com>
2024-07-18 21:32:14 +0530

    added new components display,displayhome and navbar and completed navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+import Display from "./components/Display";
 
 function App() {
- 
   return (
-    
-   <div className='h-screen bg-black'>
-<div className='h-[90%] flex'>
-<Sidebar/>
-</div>
-<div>
-  <Player/>
-</div>
-   </div>
-  )
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display/>
+      </div>
+      <div>
+        <Player />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
