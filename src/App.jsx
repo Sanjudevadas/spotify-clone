6ebc1d47d3b87a8592c086e619d0e84fab7515f3
Sanju Devadas <sanjudevadas11@gmail.com>
2024-07-23 21:10:03 +0530

    adding the functionalities to play the song

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Display from "./components/Display";
+import { useContext } from "react";
+import { PlayerContext } from "./context/PlayerContext";
 
 function App() {
+
+const {audioRef ,track} = useContext(PlayerContext);
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
@@ -14,6 +18,7 @@ function App() {
       </div>
       <div>
         <Player />
+        <audio ref={audioRef} src={track.file} preload="auto"></audio>
       </div>
     </div>
   );
